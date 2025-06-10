91895594bfc95b951f12d7ca1a5e4281490eaf8c
GriimmReaper <karanmehta2333@gmail.com>
2025-06-10 15:09:20 +0530

    changes

---
+++ 01_basic/08_dates.js
@@ -0,0 +1,41 @@
+// Date
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(typeof myDate);
+
+
+// let myCreateDate= new Date(2025, 0, 23)
+// let myCreateDate= new Date(2025, 0, 23, 5 ,3)
+// let myCreateDate= new Date("2023-01-14")
+let myCreateDate= new Date("01-14-2025")
+
+// console.log(myCreateDate.toLocaleDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+    console.log(newDate);
+    console.log(newDate.getDay());
+    console.log(newDate.getMonth());
+
+     `${newDate.getDate()}`
+
+     newDate.toLocaleString('default',{ 
+        weekday:"long",
+        
+      })
+
+    
+
+
+
+
+
+
